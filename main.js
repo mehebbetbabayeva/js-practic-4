335ad4e8950d8d977369c0f045e289ff169e146b
mehebbetbabayeva <mhbbtbabaeva97@gmail.com>
2023-01-15 20:30:42 +0400

    add function

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
+function newFunc(){
 let polidrom=prompt("eded ve ya soz daxil edin");
 let word=polidrom.split("").reverse().join("");
 if(polidrom==word){
     console.log("polidramdir")
 }else{
     console.log("polidram deyil")
+} 
 }
+newFunc();
+
 
 // polidram yoxlama
