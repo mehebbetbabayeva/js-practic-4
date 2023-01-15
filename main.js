639e3b9d8c86bb2d93e1927a91130104ee9d04b9
mehebbetbabayeva <mhbbtbabaeva97@gmail.com>
2023-01-15 20:21:53 +0400

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+let polidrom=prompt("eded ve ya soz daxil edin");
+let word=polidrom.split("").reverse().join("");
+if(polidrom==word){
+    console.log("polidramdir")
+}else{
+    console.log("polidram deyil")
+}
+
+// polidram yoxlama
